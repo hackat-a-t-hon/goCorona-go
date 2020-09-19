88509c93edee327432d9603a5b873d59d6124c85
Kartikey <rawatkari554@gmail.com>
2020-09-19 23:08:46 +0530

    Added the assests

---
+++ src/assets/level1/grass_and_water.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="grass_and_water" tilewidth="32" tileheight="32" tilecount="96" columns="8">
+ <image source="grass_and_water.png" width="256" height="384"/>
+</tileset>
